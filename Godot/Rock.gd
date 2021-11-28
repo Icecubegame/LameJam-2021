f056e476dfb5b6ebe83064a2da360c8dff70bf40
SafakOnol <94923569+SafakOnol@users.noreply.github.com>
2021-11-28 09:58:22 -0500

    Level_design_with_collision_fixes

--- Godot/Rock.gd
+++ Godot/Rock.gd
@@ -3,10 +3,12 @@ extends Node2D
 
 	
 func _on_Area2D_body_entered(body):
-	
+	print("collision")
 	if body is KinematicBody2D:
 		var boulder_scene = load("res://Boulder.tscn")
 		var boulder = boulder_scene.instance()
-		add_child(boulder)
-		boulder.set_position($Position2D.get_position())
+		get_node("/root").add_child(boulder)
+		boulder.set_position(Vector2(1573, -587))
 		$Area2D.queue_free()
+
+
