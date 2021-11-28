c6dd917a5ef4223cbd2195d244986b4fa0ecaf8e
Icecubegame <icecubegame@gmail.com>
2021-11-28 10:34:33 -0500

    added rigidbody spawner

---
+++ Godot/LevelElements/RigidBodySpawner.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+export var rigidBody2D : Resource
+export(int, 1, 100) var max_elements = 1
+var oldest_element
+var elements
+var elements_node
+var timer
+
+func _ready():
+	elements = []
+	oldest_element = 0
+	elements_node = $elements
+	timer = $Timer
+
+func _on_Timer_timeout():
+	var new_rigid = rigidBody2D.instance()
+	elements_node.add_child(new_rigid)
+	elements.append(new_rigid)
+	if elements.size() > max_elements:
+		var element = elements[0]
+		elements.erase(element)
+		element.queue_free()
+
+
+	
+	
