0907a05e152639b518aa2fdaf61d15b52412b9aa
Icecubegame <icecubegame@gmail.com>
2021-12-02 07:41:34 -0500

    added inspector settings to Click to Capture

--- Godot/Picture/Scenes/Scripts/Click_To_Capture.gd
+++ Godot/Picture/Scenes/Scripts/Click_To_Capture.gd
@@ -1,15 +1,19 @@
 extends Control
 
-var picture_scene
+export var aspect_ratio : float = 1
+export var size : float = 1
+
+onready var picture_scene = load(PictureGlobalConfig.PICTURE_PATH)
 
 func _ready():
 	set_position(get_node("/root").get_child(0).get_position())
-	picture_scene = load(PictureGlobalConfig.PICTURE_PATH)
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if Input.is_action_pressed("Capture"):
 			var picture = picture_scene.instance()
+			picture.aspect_ratio = aspect_ratio
+			picture.size = size
 			picture.set_position(get_global_mouse_position())
 			print(get_global_mouse_position())
 			add_child(picture)
